cda2d5a59ee2ab3e4c8cd0cc41081ca13f5554a8
mKnvlv <konovalovmusha@gmail.com>
2023-04-23 16:18:15 +0300

    templates structure

---
+++ script.js
@@ -0,0 +1,232 @@
+const data = {
+    "sources": [{"id": 1, "name": "meduza"}, {"id": 6, "name": "3dnews"}, {"id": 7, "name": "nytimes"}, {
+        "id": 8,
+        "name": "forbes"
+    }, {"id": 9, "name": "igromania"}, {"id": 10, "name": "buro237"}, {"id": 11, "name": "rusvesna"}, {
+        "id": 12,
+        "name": "7ya"
+    }, {"id": 14, "name": "aif"}, {"id": 15, "name": "gazetaru"}, {"id": 16, "name": "karpov.courses"}],
+    "categories": [{"id": 1, "name": "tech"}, {"id": 2, "name": "sport"}, {"id": 3, "name": "fashion"}, {
+        "id": 4,
+        "name": "politics"
+    }, {"id": 5, "name": "other"}, {"id": 6, "name": "karpov.courses"}],
+    "items": [{
+        "id": 341927,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:54:57 GMT+0300 (Moscow Standard Time)",
+        "title": "Собянин рассказал, как «Фабрика дизайна» помогает компаниям на фоне санкций",
+        "description": "Проект появился в 2022 году.",
+        "image": "https://aif-s3.aif.ru/images/032/086/25d3d46a885561d6604dea97dbdbe906.jpg",
+        "source_id": 14,
+        "category_id": 5
+    }, {
+        "id": 341925,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:48:06 GMT+0300 (Moscow Standard Time)",
+        "title": "Дизайнер квестов CD Projekt о ААА-играх: «Мы бежим к стене и скоро врежемся в неё»",
+        "description": "Ветераны игровой индустрии обсудили высокобюджетные игры.",
+        "image": "https://cdn.igromania.ru/mnt/news/6/f/5/6/a/d/125651/b08e5db89fa81cc3_848x477.jpg",
+        "source_id": 9,
+        "category_id": 1
+    }, {
+        "id": 341949,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:46:00 GMT+0300 (Moscow Standard Time)",
+        "title": "Хакеры похитили у пользователей криптовалютного кошелька Trust Wallet $170 тыс.",
+        "description": "Злоумышленники использовали уязвимость в браузерном расширении криптовалютного кошелька Trust Wallet для кражи $170 тыс. у клиентов сервиса. Сообщение об этом опубликовали разработчики криптокошелька, пообещавшие возместить ущерб.",
+        "image": "https://3dnews.ru/assets/external/illustrations/2023/04/22/1085512/261.jpg",
+        "source_id": 6,
+        "category_id": 1
+    }, {
+        "id": 341931,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:40:31 GMT+0300 (Moscow Standard Time)",
+        "title": "3 азербайджанских военнослужащих подорвались на мине в Карабахе",
+        "description": "Инцидент произошел около села Аллахгулулар в Шушинском районе.",
+        "image": "https://aif-s3.aif.ru/images/032/085/cc1d2ffc01f2e8fa11acd5d4097d6595.jpg",
+        "source_id": 14,
+        "category_id": 5
+    }, {
+        "id": 341922,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:32:25 GMT+0300 (Moscow Standard Time)",
+        "title": "«Нафтогаз Украины» заявил о переговорах с крупнейшими энергетическими компаниями США",
+        "description": "Глава «Нафтогаза Украины» в США провел переговоры с крупнейшими энергетическими компаниями страны — ExxonMobil, Halliburton и Chevron по поводу проектов в Украине. Украинские власти стремятся привлечь иностранных инвесторов в энергетический сектор, пострадавший от российской «спецоперации». В будущем украинские проекты помогут Европе заместить поставки из России, отметила FT",
+        "image": "",
+        "source_id": 8,
+        "category_id": 5
+    }, {
+        "id": 341933,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:26:30 GMT+0300 (Moscow Standard Time)",
+        "title": "Сын Пескова получил медаль «За отвагу» во время службы в зоне СВО",
+        "description": "Сын представителя Кремля работал с РСЗО «Ураган».",
+        "image": "https://aif-s3.aif.ru/images/032/085/eae7ea3b47597738a8ce5d5ac0b0aa4e.jpg",
+        "source_id": 14,
+        "category_id": 5
+    }, {
+        "id": 341943,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:23:50 GMT+0300 (Moscow Standard Time)",
+        "title": "Мексика продала Таджикистану президентский Boeing за $92 млн",
+        "description": "Правительство Таджикистана приобрело у властей Мексики президентский самолет Boeing 787-8 Dreamliner. Стоимость воздушного судна составила 1,66 млрд песо ($92 млн), сообщил в своем Twitter глава мексиканского государства Андрес Мануэль ...",
+        "image": "https://img.gazeta.ru/files3/617/16598617/Untitled-1-pic905-895x505-58223.jpg",
+        "source_id": 15,
+        "category_id": 4
+    }, {
+        "id": 341945,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:13:58 GMT+0300 (Moscow Standard Time)",
+        "title": "Замглавы ФНЭБ связал с политикой США переход РФ на нацвалюты при расчетах за энергоресурсы",
+        "description": "Переход России на национальные валюты при расчете за энергоресурсы связан с систематическим и чрезмерным злоупотреблением США своим статусом эмитента мировой резервной валюты. Такое мнение выразил заместитель генерального директора Фонда ...",
+        "image": "https://img.gazeta.ru/files3/255/15424255/RIAN_2573827.HR-pic905-895x505-29464.jpg",
+        "source_id": 15,
+        "category_id": 4
+    }, {
+        "id": 341935,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:08:23 GMT+0300 (Moscow Standard Time)",
+        "title": "Вооруженные силы Судана остановили мятеж в стране",
+        "description": "Однако вооруженные столкновения продолжаются.",
+        "image": "https://aif-s3.aif.ru/images/032/085/241f39af112e17748c2338233e3168bf.jpg",
+        "source_id": 14,
+        "category_id": 5
+    }, {
+        "id": 341946,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:01:28 GMT+0300 (Moscow Standard Time)",
+        "title": "\"Энергоатом\" договорился с американской компанией о строительстве 20 атомных энергоблоков",
+        "description": "\"Энергоатом\" и американская Holtec International заключили соглашение о строительстве на Украине до 20 атомных энергоблоков с реакторами SMR-160, сообщает РБК, ссылаясь на пресс-службу украинской компании.\n\nСообщается, что ...",
+        "image": "https://img.gazeta.ru/files3/523/11832523/nuclear-pic905-895x505-21764.jpg",
+        "source_id": 15,
+        "category_id": 4
+    }, {
+        "id": 341924,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 19:00:00 GMT+0300 (Moscow Standard Time)",
+        "title": "Очередной шаг к чемпионству: \"Зенит\" принимает \"Динамо\". LIVE",
+        "description": "Петербургский \"Зенит\", который лидирует в турнирной таблице национального первенства, в 24-м туре Российской премьер-лиги (РПЛ) принимает дома московское \"Динамо\". \"Газета.Ru\" ведет текстовую онлайн-трансляцию поединка.",
+        "image": "https://img.gazeta.ru/files3/323/16403323/RIA_8391189-pic905-895x505-80476.jpg",
+        "source_id": 15,
+        "category_id": 2
+    }, {
+        "id": 341929,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 18:50:16 GMT+0300 (Moscow Standard Time)",
+        "title": "«Братья Супер Марио в кино» поставили новый рекорд кассовых сборов",
+        "description": "За третий уикенд они заработали 58 миллионов долларов.",
+        "image": "https://cdn.igromania.ru/mnt/news/a/8/f/0/7/5/125650/700eeedc0bc87cb4_848x477.jpg",
+        "source_id": 9,
+        "category_id": 1
+    }, {
+        "id": 341936,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 18:46:51 GMT+0300 (Moscow Standard Time)",
+        "title": "Николай Песков рассказал о реакции отца на решение отправиться в зону СВО",
+        "description": "Представитель Кремля полностью поддержал инициативу сына.",
+        "image": "https://aif-s3.aif.ru/images/032/085/bfe9a4415a674485630794631f96644d.jpg",
+        "source_id": 14,
+        "category_id": 4
+    }, {
+        "id": 341938,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 18:40:00 GMT+0300 (Moscow Standard Time)",
+        "title": "В Гетеборге люди протестуют против вступления Швеции в НАТО",
+        "description": "В центр города пришли сотни человек.",
+        "image": "https://aif-s3.aif.ru/images/032/085/d36592215dad21bbc450f97350465279.jpg",
+        "source_id": 14,
+        "category_id": 5
+    }, {
+        "id": 341947,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 18:27:14 GMT+0300 (Moscow Standard Time)",
+        "title": "\"Уралхим-Уралкалий\" отправил партию удобрений в 34 тыс. тонн из латвийского порта в Кению",
+        "description": "Специализирующаяся на азотных и фосфорных удобрениях российская группа \"Уралхим-Уралкалий\" отправила из Риги партию удобрений объемом 34 тыс. тонн в рамках гуманитарной помощи в Кению. Сообщение об этом опубликовала пресс-служба ...",
+        "image": "https://img.gazeta.ru/files3/224/14727224/20220308_zaa_s197_105-pic905-895x505-10346.jpg",
+        "source_id": 15,
+        "category_id": 4
+    }, {
+        "id": 341951,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 18:27:00 GMT+0300 (Moscow Standard Time)",
+        "title": "Карты 2ГИС снова появились в App Store",
+        "description": "Популярное картографическое приложение 2ГИС снова доступно для скачивания в магазине цифрового контента Apple App Store. Об этом сообщило информационное агентство ТАСС со ссылкой на данные пресс-службы сервиса. Также было сказано, что в ближайшее время будет выпущено обновление данного приложения.",
+        "image": "https://3dnews.ru/assets/external/illustrations/2023/04/22/1085504/260.jpg",
+        "source_id": 6,
+        "category_id": 1
+    }, {
+        "id": 341939,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 18:25:01 GMT+0300 (Moscow Standard Time)",
+        "title": "Сын Пескова рассказал, почему решил пройти службу в ЧВК «Вагнер»",
+        "description": "Николай Песков назвал своим долгом помощь в зоне СВО.",
+        "image": "https://aif-s3.aif.ru/images/032/085/dfda14281e21ee1f03002f33c4d83269.JPG",
+        "source_id": 14,
+        "category_id": 5
+    }, {
+        "id": 341932,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 18:18:21 GMT+0300 (Moscow Standard Time)",
+        "title": "Новый временный режим Apex Legends «Глаз смерти» станет испытанием мастерства",
+        "description": "Коллекционное событие «Вуаль» пройдёт с 25 апреля по 9 мая.",
+        "image": "https://cdn.igromania.ru/mnt/news/d/c/d/8/a/9/125649/9f2df99fb0844746_848x477.jpg",
+        "source_id": 9,
+        "category_id": 1
+    }, {
+        "id": 341940,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 18:17:17 GMT+0300 (Moscow Standard Time)",
+        "title": "На Украине предлагают сажать в тюрьму за критику власти в соцсетях",
+        "description": "Законопроект предусматривает от 5 до 7 лет лишения свободы и конфискацию имущества.",
+        "image": "https://aif-s3.aif.ru/images/032/085/5a5ff0d8a2d0effb84e81530af7c504f.jpg",
+        "source_id": 14,
+        "category_id": 5
+    }, {
+        "id": 341941,
+        "lang": "ru",
+        "date": "Sat Apr 22 2023 18:13:07 GMT+0300 (Moscow Standard Time)",
+        "title": "Прихожане УПЦ пожаловались ЕС и ООН на украинскую власть",
+        "description": "Верующие попросили обратить внимание на дискриминацию по религиозному признаку.",
+        "image": "https://aif-s3.aif.ru/images/032/085/bd54447c49ed7122db24774472efff10.jpg",
+        "source_id": 14,
+        "category_id": 5
+    }]
+}
+
+const mainNews = data.items.slice(0, 3)
+const smallNews = data.items.slice(3, 12)
+
+const mainNewsTemplate = document.querySelector(".main-news-item")
+const smallNewsTemplate = document.querySelector(".small-news-item")
+const mainNewsContainer = document.querySelector(".articles__big-column")
+const smallNewsContainer = document.querySelector(".articles__small-column")
+
+
+mainNews.forEach((item) => {
+
+    const element =  mainNewsTemplate.content.cloneNode(true)
+
+    const source = data.sources.find((it) => it.id === item.source_id)
+    const category = data.categories.find((it) => it.id === item.category_id)
+
+    element.querySelector(".main-article__title").textContent = item.title
+    element.querySelector(".main-article__text").textContent = item.description
+    element.querySelector(".main-article__source").textContent = source.name
+    element.querySelector(".main-article__category").textContent = category.name
+    element.querySelector(".main-article__image").src = item.image
+
+    mainNewsContainer.appendChild(element)
+
+})
+smallNews.forEach((item) => {
+    const element = smallNewsTemplate.content.cloneNode(true)
+
+    const date = new Date(item.date).toLocaleDateString("ru-RU",{month: "long", day: "numeric"})
+    const source = data.sources.find((it) => it.id === item.source_id)
+
+    element.querySelector(".small-article__title").textContent = item.title
+    element.querySelector(".small-article__date").textContent = date
+    element.querySelector(".small-article__source").textContent = source.name
+
+    smallNewsContainer.appendChild(element)
+})
